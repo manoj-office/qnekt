53375cd9e0173ef2f12870ca5b283d3996ef8a37
Manojkumar <155512659+manoj-office@users.noreply.github.com>
2025-04-18 12:44:07 +0530

    schema fix

--- schema/tableSchema.js
+++ schema/tableSchema.js
@@ -12,6 +12,7 @@ const categorySchema = new mongoose.Schema(
         instructor_id: { type: String },
         price: { type: String },
         isFeature: { type: Boolean, default: false },
+        isFooter: { type: Boolean, default: false },
         // category_id: { type: String }, 	
         // created_at: { type: Date },
         lastActive: { type: Date },
